126b85a58a644b616b3986fdf4289bb1b46afc2d
darshilkantaria11 <91153906+darshilkantaria11@users.noreply.github.com>
2022-09-20 11:54:33 +0530

    Update app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,4 @@
+alert("NOTE:- Password should contain minimum 8 character which include Uppercase,lowercase and Special character");
 let password = document.getElementById('password');
 let showHideBtn = document.getElementById('show-hide');
 
@@ -12,5 +13,22 @@ function showHide() {
     showHideBtn.innerHTML = 'show';
   }
 }
+
+
+
+// function validateform(){  
+//   var name=document.myform.name.value;  
+//   var password=document.myform.password.value;  
+    
+//   if (name==null || name==""){  
+//     alert("Name can't be blank");  
+//     return false;  
+//   }else if(password.length<6){  
+//     alert("Password must be at least 6 characters long.");  
+//     return false;  
+//     }  
+//   }  
+
+
 
 showHideBtn.onclick = showHide;
